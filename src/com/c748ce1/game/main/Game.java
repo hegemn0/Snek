6cece00cf0ef5410c8e2503bcc9cdfdc84467a8f
hegemn0 <hegemn0@DESKTOP-SEK4F71>
2016-10-14 13:42:31 -0400

    Teste commint.

--- src/com/c748ce1/game/main/Game.java
+++ src/com/c748ce1/game/main/Game.java
@@ -49,11 +49,12 @@ public class Game extends JPanel implements Runnable {
         setFocusable(true);
         requestFocus(); 
     }    
-
+    
     @Override
     public void run() {
         long duracaoAtualizacao = 0;
         long duracaoRepouso = 0;
+        int numeroTeste = 0;
                 
         while(executando) {
             long antesAtualizacao = System.nanoTime();
